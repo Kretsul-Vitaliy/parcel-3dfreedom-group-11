fae8873c0a269a9cd153279c824c18596b948771
viikkii <viikkii@users.noreply.github.com>
2021-08-16 23:10:38 +0000

    Deploying to gh-pages from @ Kretsul-Vitaliy/parcel-3dfreedom-group-11@45a8210b2c0caa09a339410ef2279fd0212bc8c8 🚀

--- contacts-page.1106e089.js
+++ contacts-page.1106e089.js
@@ -5,4 +5,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"hGbe":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./js/menu");
 },{"./sass/main.scss":"clu1","./js/menu":"i0CD"}]},{},["hGbe"], null)
-//# sourceMappingURL=/parcel-3dfreedom-group-11/contacts-page.23b483ad.js.map
+//# sourceMappingURL=/parcel-3dfreedom-group-11/contacts-page.1106e089.js.map
--- src.57754e4a.js
+++ src.57754e4a.js
@@ -7,4 +7,4 @@ $(document).ready(function(){$(".zoom-gallery").magnificPopup({delegate:"a",type
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./js/gallery"),require("./js/menu");
 },{"./sass/main.scss":"clu1","./js/gallery":"chxP","./js/menu":"i0CD"}]},{},["Focm"], null)
-//# sourceMappingURL=/parcel-3dfreedom-group-11/src.e43b9edf.js.map
+//# sourceMappingURL=/parcel-3dfreedom-group-11/src.57754e4a.js.map
