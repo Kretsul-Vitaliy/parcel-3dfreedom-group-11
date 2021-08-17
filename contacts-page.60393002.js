7836102e5af052f3ab9b63a1725acdb3624c108f
Yakov-Homylev <Yakov-Homylev@users.noreply.github.com>
2021-08-17 12:20:38 +0000

    Deploying to gh-pages from @ Kretsul-Vitaliy/parcel-3dfreedom-group-11@9715e20fd58ac50fc760efcf958443b0e409ca54 🚀

--- contacts-page.60393002.js
+++ contacts-page.60393002.js
@@ -5,4 +5,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"hGbe":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./js/menu");
 },{"./sass/main.scss":"clu1","./js/menu":"i0CD"}]},{},["hGbe"], null)
-//# sourceMappingURL=/parcel-3dfreedom-group-11/contacts-page.cbc1ad71.js.map
+//# sourceMappingURL=/parcel-3dfreedom-group-11/contacts-page.60393002.js.map
