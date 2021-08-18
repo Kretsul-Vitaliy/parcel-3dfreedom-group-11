9048d117bdd949c2a892c1c062fd41f7311ac174
SerheijTsotsorin <sergcocorin61@gmail.com>
2021-08-19 00:15:31 +0300

    header and mobile menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,5 +1,20 @@
-$(document).ready(function() {
-    $('.open-btn').click(function(event) {
-        $(`.mobile`).toggleClass(`active`);
-    })
-})
+// $(document).ready(function() {
+//     $('.open-btn').click(function(event) {
+//         $(`.mobile`).toggleClass(`active`);
+//     })
+// })
+
+(() => {
+    const refs = {
+      openMenuBtn: document.querySelector("[data-menu-open]"),
+      closeMenuBtn: document.querySelector("[data-menu-close]"),
+      menu: document.querySelector("[data-menu]"),
+    };
+  
+    refs.openMenuBtn.addEventListener("click", toggleModal);
+    refs.closeMenuBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.menu.classList.toggle("open");
+    }
+  })();
