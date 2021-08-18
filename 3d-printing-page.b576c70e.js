5e88289ece6544509a1cd82c3fafe35d6af88324
Kretsul-Vitaliy <Kretsul-Vitaliy@users.noreply.github.com>
2021-08-18 04:19:38 +0000

    Deploying to gh-pages from @ Kretsul-Vitaliy/parcel-3dfreedom-group-11@0bacdf7a421ded7a514bee8373d8a3bace8ad3c7 🚀

--- 3d-printing-page.b576c70e.js
+++ 3d-printing-page.b576c70e.js
@@ -5,4 +5,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"isWM":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./js/menu");
 },{"./sass/main.scss":"clu1","./js/menu":"i0CD"}]},{},["isWM"], null)
-//# sourceMappingURL=/parcel-3dfreedom-group-11/3d-printing-page.b216564c.js.map
+//# sourceMappingURL=/parcel-3dfreedom-group-11/3d-printing-page.b576c70e.js.map
