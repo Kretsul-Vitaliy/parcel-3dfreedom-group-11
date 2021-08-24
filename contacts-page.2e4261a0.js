eea595905afb732bb4899475ffac7087186c9b47
viikkii <viikkii@users.noreply.github.com>
2021-08-24 21:20:05 +0000

    Deploying to gh-pages from @ Kretsul-Vitaliy/parcel-3dfreedom-group-11@3317e26204c5a913bd607fe71a52b42d5ac254c0 🚀

--- contacts-page.2e4261a0.js
+++ contacts-page.2e4261a0.js
@@ -5,4 +5,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"hGbe":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./js/menu");
 },{"./sass/main.scss":"clu1","./js/menu":"i0CD"}]},{},["hGbe"], null)
-//# sourceMappingURL=/parcel-3dfreedom-group-11/contacts-page.d89d9ab7.js.map
+//# sourceMappingURL=/parcel-3dfreedom-group-11/contacts-page.2e4261a0.js.map
